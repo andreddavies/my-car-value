908287cbfa08324c4f106eb881bae08ddfc92d9a
André Davies <andredavies.dev@gmail.com>
2025-06-05 20:19:10 -0300

    implement signup method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
 import { SignupDto } from './dto/signup.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   signup({ email, password }: SignupDto) {
-    console.log(`Email: ${email} || Password: ${password}`);
+    const user = this.userRepository.create({ email, password });
+
+    return this.userRepository.save(user);
   }
 }
