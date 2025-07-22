70fea82ce745dca42b71c905b75d68d587143a95
André Davies <andredavies.dev@gmail.com>
2025-07-22 02:08:10 -0300

    feat: add new report routes

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,6 +1,22 @@
-import { Controller, Delete, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
 import { ReportsService } from './reports.service';
+import { CreateReportDto } from './dto/create-report.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ReportDto } from './dto/report.dto';
+import { ApproveReportDto } from './dto/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -14,6 +30,18 @@ export class ReportsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.reportsService.findOne(+id);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  create(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(id, body.approved);
   }
 
   @Delete(':id')
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,13 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './entities/report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dto/create-report.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class ReportsService {
+  constructor(
+    @InjectRepository(Report)
+    private readonly reportsRepository: Repository<Report>,
+  ) {}
+
   findAll() {
     return `This action returns all reports`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} report`;
+  }
+
+  create(payload: CreateReportDto, user: User) {
+    const report = this.reportsRepository.create(payload);
+    report.user = user;
+
+    return this.reportsRepository.save(report);
+  }
+
+  async changeApproval(id: string, payload: boolean) {
+    const report = await this.reportsRepository.findOne({
+      where: { id: parseInt(id) },
+    });
+
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+
+    report.approved = payload;
+    return this.reportsRepository.save(report);
   }
 
   remove(id: number) {
