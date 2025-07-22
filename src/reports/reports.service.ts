c9217294c6ccdec8c745ac0e08167d4d4347fe68
André Davies <andredavies.dev@gmail.com>
2025-07-22 02:54:49 -0300

    feat: create service to get estimate price

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Report } from './entities/report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dto/create-report.dto';
 import { User } from 'src/users/entities/user.entity';
+import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -38,6 +39,23 @@ export class ReportsService {
 
     report.approved = payload;
     return this.reportsRepository.save(report);
+  }
+
+  createEstimate(query: GetEstimateDto) {
+    const { make, model, lat, lng, mileage, year } = query;
+
+    return this.reportsRepository
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make })
+      .andWhere('model = :model', { model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC', 'NULLS LAST')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
   }
 
   remove(id: number) {
